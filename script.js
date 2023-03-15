f5e7486ab37acf07fbad91b3a83121bfad38073a
Adrian Lucero <adrian.9708@gmail.com>
2023-03-15 20:58:39 -0300

    primera entrega JS

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcularPrecio (precioAlimento, cantidadAlimento, porcentajeDescuento, costoEnvio) {
+    let descuento = (precioAlimento * porcentajeDescuento) / 100;
+    let precioConDescuento = precioAlimento - descuento;
+    return (precioConDescuento * cantidadAlimento) + costoEnvio;
+}
+
+const envio = 300;
+
+let alimento = parseFloat(prompt("Ingrese el monto del alimento"));
+let cantidad = parseInt(prompt("Ingrese la cantidad de bolsones que necesita"));
+let descuento = parseInt(prompt("Ingresa el porcentaje de descuento que figura en la tarjeta")) 
+
+let precioFinal = calcularPrecio(alimento, cantidad, descuento, envio);
+alert("El precio total de tu compra es $" + precioFinal);
+alert("¡Gacias, cuidemos a los animalitos!");
